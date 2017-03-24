19f9837a568510bcb252bffd47a4fdfc9539ebc5
Brian Graham <drakain@gmail.com.com>
2017-03-24 15:40:08 -0400

    Download all repos in csv

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
-const csv = require('csv-streamify')
-const fs = require('fs')
+var through2Concurrent = require('through2-concurrent');
+const Git = require("nodegit");
 
-const parser = csv({ objectMode: true }, function (err, result) {
+const csv = require('csv-streamify');
+const fs = require('fs');
+
+const parser = csv({ objectMode: true });
+
+const file="clean_projects.csv";
+
+const nodegitOptions = {
+  fetchOpts: {
+    callbacks: {
+        certificateCheck: () => { return 1; },
+        credentials: function(url, userName) {
+          const cred = Git.Cred.sshKeyFromAgent(userName);
+          return cred;
+        }
+    }
+  }
+};
+
+let all = [];
+fs.createReadStream(file)
+.pipe(parser)
+.pipe(through2Concurrent.obj(
+  {maxConcurrency: 100},
+  function (line, enc, done) {
+    const self = this;
+    const repo_api_path = line[1];
+    const repo_name = repo_api_path.replace("https://api.github.com/repos/", "");
+    const repo_git_address = "git@github.com:" + repo_name;
+    const repo_download = Git.Clone(repo_git_address, "../repos/" + repo_name, nodegitOptions);
+    repo_download.then(
+      function(resolved) {
+        console.log(resolved, repo_name);
+      },
+      function(rejected) {
+        console.log(rejected, repo_name);
+      }
+    )
+    repo_download.then(done)
+    self.push(repo_download)
+
+}))
+.on('data', function (data) {
+  all.push(data)
+  console.log("***IN DATA", data)
 })
-
-// emits each line as a buffer or as a string representing an array of fields
-parser.on('data', function (line) {
-  const repo_api_path = line[1];
-  const repo_name = repo_api_path.replace("https://api.github.com/repos/", "")
-
-  console.log(repo_name);
+.on('end', function () {
+  console.log(all)
 })
-
-// projects.csv is from http://ghtorrent.org/
-fs.createReadStream('../projects.csv').pipe(parser)
